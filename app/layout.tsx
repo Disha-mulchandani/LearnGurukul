d416198a8c2aa37d5d21b403b79059e70e60c058
Disha Mulchandani <disha.mulchandani@example.com>
2026-07-02 17:03:02 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import Script from "next/script";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
@@ -72,6 +72,20 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
+        <Script
+  src={`https://www.googletagmanager.com/gtag/js?id=G-PNGS9C2HXH`}
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PNGS9C2HXH');
+`}
+</Script>
       </body>
     </html>
   );
